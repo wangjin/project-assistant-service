bfde397959b8604deb4acdcf5f6448f73295b46f
Jin Wang <15305187826@189.cn>
2021-09-01 18:38:40 +0800

    初始导入

---
+++ src/main/java/com/codebetterlife/projectassistant/ProjectAssistantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codebetterlife.projectassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAssistantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAssistantServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codebetterlife/projectassistant/ProjectAssistantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebetterlife.projectassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAssistantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
